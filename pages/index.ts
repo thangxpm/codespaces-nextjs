6c824c523d009eefd445737cf68646926af170a0
Thang Nguyen <105143516+thangxpm@users.noreply.github.com>
2023-03-01 03:21:31 +0000

    first commit

--- pages/index.ts
+++ pages/index.ts
@@ -1,7 +1,4 @@
 import { useCallback, useEffect, useState } from 'react'
-import Button from '../components/Button'
-import ClickCount from '../components/ClickCount'
-import styles from '../styles/home.module.css'
 
 function throwError() {
   console.log(
